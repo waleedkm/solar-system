8d35eb8356b3dfd56e0bdc6795800a7d09c7504e
waleedkm <33782406+waleedkm@users.noreply.github.com>
2024-09-03 21:02:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+#    user: process.env.MONGO_USERNAME,
+  #  pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
